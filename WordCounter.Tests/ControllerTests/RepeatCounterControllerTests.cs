9185c1e57d35fe0a9b178fdc7e77740f1e725842
Lan Dam <ldam77@yahoo.com>
2018-07-06 13:51:36 -0700

    add tests for httpget for index of home and repeatcounter controllers

---
+++ WordCounter.Tests/ControllerTests/RepeatCounterControllerTests.cs
@@ -0,0 +1,37 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WordCounter.Controllers;
+using WordCounter.Models;
+
+namespace WordCounter.Tests
+{
+    [TestClass]
+    public class RepeatCountersControllerTest
+    {
+      [TestMethod]
+        public void Index_ReturnsCorrectView_True()
+        {
+            //Arrange
+            RepeatCountersController controller = new RepeatCountersController();
+
+            //Act
+            ActionResult indexView = controller.Index();
+
+            //Assert
+            Assert.IsInstanceOfType(indexView, typeof(ViewResult));
+        }
+        [TestMethod]
+        public void Index_HasCorrectModelType_ItemList()
+        {
+            //Arrange
+            ViewResult indexView = new RepeatCountersController().Index() as ViewResult;
+
+            //Act
+            var result = indexView.ViewData.Model;
+
+            //Assert
+            Assert.IsInstanceOfType(result, typeof(List<RepeatCounter>));
+        }
+    }
+}
